40a8e3d95fbeae936e938e8adf55731d688a975b
mark <markalessi@fullsail.edu>
2013-03-22 14:43:18 -0400

    restart initial

---
+++ main.js
@@ -0,0 +1,45 @@
+// main.js
+// Mark Alessi
+// VFW 1303
+// Project 3 
+
+		//form validation
+
+// function checkForm() {
+// document.form.Email.value = " " + document.form.Email.value;
+
+// focus events 
+
+var movie = document.getElementById("movie");
+var info = document.getElementById("info");
+
+var addBorder = function() {
+	movie.setAttribute("class", "hasFocus");
+}
+
+movie.addEventListener("focus", "addBorder");
+
+		//verify that name field is not blank
+ 
+if (document.form.Name.value == "") {                      
+alert('Please enter your name.'); return false; }
+     
+		//verify that email format is valid
+
+mail = document.form.Email.value;                            
+if ((mail.indexOf('@') == -1) || (mail.indexOf('.') == -1)) {
+alert('This E-mail address is invalid'); return false; } 
+
+		//verify a rating is selected
+
+if (document.form.Rate.selectedIndex == " ") {             
+alert('Please help me by rating my Site'); return false; } 
+
+		//verify comment box is not empty
+ 
+if (document.form.Query.value == "") {                       
+alert('Please add a question or comment.'); return false; }
+ 
+}
+ 
+</script> 
