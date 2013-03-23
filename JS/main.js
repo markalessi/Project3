24041fe7fcded251f48dfdb1cb8f4a08c51b00cc
mark <markalessi@fullsail.edu>
2013-03-22 22:45:02 -0400

    update

--- JS/main.js
+++ JS/main.js
@@ -64,3 +64,56 @@ saveData();
 document.getElementById("clear").onclick = function () {
     var clearStorage = localStorage.clear()
 };
+
+// form validation
+
+function validateForm(){
+
+
+		if ( document.form1.movietitle.value == "" )
+    {
+        alert ( "Please fill in the title box." );
+        valid = false;
+    }
+    	return valid;
+
+			if ( document.checkbox.genre.checked == false )
+	    {
+	        alert ( "Please check at least one genre box." );
+	        valid = false;
+	    }
+	    	return valid;
+
+	    		if ( document.form1.rating.selectedIndex == 0 )
+    {
+		        alert ( "You are required to select a rating." );
+		        valid = false;
+			    }
+				return valid;
+}
+
+validateForm()
+
+// write daata from local storage
+
+function getData(){
+	var makeDiv = document.createElement("div");	
+	makeDiv.setAttribute("id", "items");
+	var makeList = document.createElement("ul");
+	makeDiv.appendChild(makeList);
+	document.body.appendChild(makeList);
+	for(var i=0, j=localStorage.length; i<j; i++){
+		var makeli = document.createElement("li");
+		makeList.appendChild(makeli);
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		var obj = JSON.parse(value);
+		var makeSubList = document.createElement("ul") 
+		makeli.appendChild(makeSubList)
+		for (var n in obj) {
+			var makeSubli = document.createElement("li");
+			var optSubText = obj[n][0]+" "+obj[n][1];
+			makeSubli.innerHTML = optSubText;
+		}
+	}	
+}	
